d54eaa55cfdd5373d38b090840e238eca2da7e5e
Hatem Salem <hasalem@gmail.com>
2018-04-22 19:40:46 +0200

    add abded mall for offer scrapers

--- devInit.bat
+++ devInit.bat
@@ -1,35 +1,23 @@
 echo off
 echo "Please stop Tomcat"
 pause
-echo "Removing resources folder"
-rd /s /q c:\zadata\resources
-echo "Removing init\hotlines"
-rd /s /q c:\zadata\init\hotlines
-echo "Removing init\cat_res"
-rd /s /q c:\zadata\init\cat_res
-echo "Removing init\res"
-rd /s /q c:\zadata\init\res
-echo "Removing init\logs"
-rd /s /q c:\zadata\logs
-echo "Removing init\jsonoffers"
-rd /s /q c:\zadata\jsonoffers
-
+echo "Removing work folder"
+rd /s /q c:\zadata\work
 echo "Kindly start up the tomcat, then press enter"
 pause
-echo "Copying init data files--Categories, Press any key to continue"
-cd \zadata
-pause
-copy c:\zadata\init\sealed\categories.csv c:\zadata\init\*.*
+echo "Copying init data files Press any key to continue"
+xcopy c:\zadata\sealed c:\zadata\work\ /e/t
+xcopy C:\zadata\sealed\categories\*.* C:\zadata\work\categories\*.* /E
 echo "Copying init data files--16xxx hotlines,Press any key to continue"
 pause
-md c:\zadata\init\hotlines
-md C:\zadata\init\cat_res
-md C:\zadata\init\res
-copy c:\hotlines\16*.txt c:\zadata\init\hotlines\*.*
-pause 
-copy c:\hotlines\19*.txt c:\zadata\init\hotlines\*.*
+xcopy C:\zadata\sealed\hotlines\16*.* C:\zadata\work\hotlines\*.* 
+xcopy C:\zadata\sealed\hotlines\issues\*16113.* C:\zadata\work\hotlines\*.* 
+xcopy C:\zadata\sealed\hotlines\issues\*16404.* C:\zadata\work\hotlines\*.* 
+xcopy C:\zadata\sealed\hotlines\issues\*16061.* C:\zadata\work\hotlines\*.* 
+xcopy C:\zadata\sealed\hotlines\issues\*16257.* C:\zadata\work\hotlines\*.* 
+echo "Copying init data files--19xxx hotlines,Press any key to continue"
 pause
-
-echo "Copying logos of companies and groups"
-xcopy C:\zadata\init\sealed\cat_res\*.* C:\zadata\init\cat_res\*.* /E
-xcopy C:\zadata\init\sealed\res\*.* C:\zadata\init\res\*.* /E
+xcopy C:\zadata\sealed\hotlines\19*.* C:\zadata\work\hotlines\*.* 
+echo "Copying Logos,Press any key to continue"
+pause
+xcopy C:\zadata\sealed\logos\*.* C:\zadata\work\logos\*.* /E
